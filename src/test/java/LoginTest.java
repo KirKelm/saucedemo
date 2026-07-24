73bf92820ebd1c2de53ae33a3b11aba588443293
Kirill Kelmachev <hekct@yandex.ru>
2026-07-25 02:45:58 +0400

    loginTest is added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -19,7 +19,6 @@ public class LoginTest {
         browser.findElement(By.xpath("//*[@data-test='login-button']")).click();
 //        browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed();
 //        assertTrue(browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed());
-//dfdg
         boolean isErrorDisplayed = browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed();
         String errorMessage = browser.findElement(By.xpath("//*[@data-test='error']")).getText();
 
