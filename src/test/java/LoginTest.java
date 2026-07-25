fa2142abb0b693c9c5052dfa3a00029c317eebb0
Kirill Kelmachev <hekct@yandex.ru>
2026-07-25 04:29:40 +0400

    valid\invalid login is added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,27 +3,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class LoginTest {
 
     @Test
-    public void login() {
+    public void validLogin() {
         WebDriver browser = new ChromeDriver();
         browser.get("https://www.saucedemo.com/");
-
         browser.findElement(By.xpath("//*[@data-test='username']")).sendKeys("standard_user");
-        browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce123");
+        browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce");
         browser.findElement(By.xpath("//*[@data-test='login-button']")).click();
-//        browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed();
-//        assertTrue(browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed());
-        boolean isErrorDisplayed = browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed();
-        String errorMessage = browser.findElement(By.xpath("//*[@data-test='error']")).getText();
 
-        assertTrue(isErrorDisplayed);
-        assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service");
+        assertTrue(browser.findElement(By.xpath("//*[@data-test='title']")).isDisplayed(),
+                "Сообщение об ошибке не отображено!");
+        assertEquals(browser.findElement(By.xpath("//*[@data-test='title']")).getText(),
+                "Products");
+
+        browser.quit();
+    }
+
+    @Test
+    public void invalidLogin() {
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.xpath("//*[@data-test='username']")).sendKeys("standard_user2");
+        browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce");
+        browser.findElement(By.xpath("//*[@data-test='login-button']")).click();
+        browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed();
+        browser.findElement(By.xpath("//*[@data-test='error']")).getText();
+
+        assertTrue(browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed());
+        assertEquals(browser.findElement(By.xpath("//*[@data-test='error']")).getText(),
+                "Epic sadface: Username and password do not match any user in this service");
 
         browser.quit();
     }
