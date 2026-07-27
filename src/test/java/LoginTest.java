bbeb29160c7bd41ebd638746e7753af0df4e1802
Kirill Kelmachev <hekct@yandex.ru>
2026-07-27 16:23:55 +0400

    LoginTest  added annotation

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,23 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class LoginTest {
+    WebDriver browser = new ChromeDriver();
+
+    @BeforeMethod
+    public void setup() {
+        browser = new ChromeDriver();
+        browser.get("https://www.saucedemo.com/");
+    }
 
     @Test
     public void validLogin() {
-        WebDriver browser = new ChromeDriver();
-        browser.get("https://www.saucedemo.com/");
         browser.findElement(By.xpath("//*[@data-test='username']")).sendKeys("standard_user");
         browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce");
         browser.findElement(By.xpath("//*[@data-test='login-button']")).click();
@@ -19,14 +26,10 @@ public class LoginTest {
                 "Сообщение об ошибке не отображено!");
         assertEquals(browser.findElement(By.xpath("//*[@data-test='title']")).getText(),
                 "Products");
-
-        browser.quit();
     }
 
     @Test
     public void invalidLogin() {
-        WebDriver browser = new ChromeDriver();
-        browser.get("https://www.saucedemo.com/");
         browser.findElement(By.xpath("//*[@data-test='username']")).sendKeys("standard_user2");
         browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce");
         browser.findElement(By.xpath("//*[@data-test='login-button']")).click();
@@ -36,14 +39,10 @@ public class LoginTest {
         assertTrue(browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed());
         assertEquals(browser.findElement(By.xpath("//*[@data-test='error']")).getText(),
                 "Epic sadface: Username and password do not match any user in this service");
-
-        browser.quit();
     }
 
     @Test
     public void emptyPassword() {
-        WebDriver browser = new ChromeDriver();
-        browser.get("https://www.saucedemo.com/");
         browser.findElement(By.xpath("//*[@data-test='username']")).sendKeys("standard_user2");
         browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("");
         browser.findElement(By.xpath("//*[@data-test='login-button']")).click();
@@ -53,7 +52,23 @@ public class LoginTest {
         assertTrue(browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed());
         assertEquals(browser.findElement(By.xpath("//*[@data-test='error']")).getText(),
                 "Epic sadface: Password is required");
+    }
 
+    @Test
+    public void emptyLogin() {
+        browser.findElement(By.xpath("//*[@data-test='username']")).sendKeys("");
+        browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce");
+        browser.findElement(By.xpath("//*[@data-test='login-button']")).click();
+        browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed();
+        browser.findElement(By.xpath("//*[@data-test='error']")).getText();
+
+        assertTrue(browser.findElement(By.xpath("//*[@data-test='error']")).isDisplayed());
+        assertEquals(browser.findElement(By.xpath("//*[@data-test='error']")).getText(),
+                "Epic sadface: Username is required");
+    }
+
+    @AfterMethod
+    public void close() {
         browser.quit();
     }
 }
